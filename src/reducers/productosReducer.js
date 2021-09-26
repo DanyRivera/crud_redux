d552d0e8b5ff077972e3ea6bcdeaec573ef94dee
Dany <dany@gmail.com>
2021-09-26 14:42:56 -0500

    Empezando con los acions para productos

---
+++ src/reducers/productosReducer.js
@@ -0,0 +1,25 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+import {
+    AGREGAR_PRODUCTO,
+    AGREGAR_PRODUCTO_EXITO,
+    AGREGAR_PRODUCTO_ERROR
+} from '../types';
+
+//cada reducer tiene su propio state
+const initialState = {
+    productos: [],
+    error: null,
+    loading: false
+}
+
+export default function( state = initialState, action) {
+
+    switch (action.type) {
+
+    
+        default:
+            return state;
+    }    
+
+}
